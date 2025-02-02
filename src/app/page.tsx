54cadf0aef2d1e2cf1c7562ecfe478b4c96eaeef
bange1cm <bange1cm@cmich.edu>
2025-02-01 19:08:38 -0500

    Changing look of start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,18 +23,36 @@ export default function RoadTripPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-6" style={{ backgroundColor: '#f2fae9' }}>
+    <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "20px",
+      height: "80vh",
+      backgroundColor: "#f9fef9",
+    }}
+    >
       {!roadTripCode ? (
         <div className="flex flex-col items-center space-y-4">
           <h1 className="text-2xl font-bold">My Cows:</h1>
           <h2 className="text-xl">Road Unwrapped</h2>
           {/* making both buttons vertically stacked */}
-
+          <div className="flex flex-col space-y-2">
+            <div style={{padding: "5px"}}> 
           <Button
             onClick={generateCode}
             className="px-4 py-2 bg-blue-500 text-white rounded-lg"
           >
             Create +
+          </Button>
+          </div>
+          <div style={{padding: "5px"}}> 
+          <Button
+            onClick={joinTrip}
+            className="px-4 py-2 bg-green-500 text-white rounded-lg"
+          >
+            Join
           </Button>
           <Input
             type="text"
@@ -43,12 +61,8 @@ export default function RoadTripPage() {
             onChange={(e) => setJoinCode(e.target.value)}
             className="border p-2 rounded-lg"
           />
-          <Button
-            onClick={joinTrip}
-            className="px-4 py-2 bg-green-500 text-white rounded-lg"
-          >
-            Join
-          </Button>
+          </div>
+          </div>
         </div>
       ) : (
         <div className="flex flex-col items-center space-y-4">
